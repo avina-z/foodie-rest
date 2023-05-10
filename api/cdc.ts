689b87c69b5f50ecaa68fec535bb9c9deae44f95
Alberto Avina <avinaz@mx1.ibm.com>
2023-05-10 12:16:14 -0600

    Fix Async

--- api/cdc.ts
+++ api/cdc.ts
@@ -1,12 +1,21 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { processRecords } from './_writeToDatabase';
+import type { Readable } from 'node:stream';
 
 //const writeToDatabase = require("./_writeToDatabase.ts");
+async function buffer(readable: Readable) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks);
+}
 
-export default (req: VercelRequest, res: VercelResponse) => {
-  const { body } = req;
-  console.log(body); 
+export default async function (req: VercelRequest, res: VercelResponse) { 
   if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const body = buf.toString('utf8');
+    console.log(body);
     processRecords(body);
     return res.json({ message: "POST: Success" });
   } else {
