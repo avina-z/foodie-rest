0046c5627f01660f2f791335314854ec71e25ef2
Alberto Avina <avinaz@mx1.ibm.com>
2023-05-09 21:09:43 -0600

    Add Logging

--- api/cdc.ts
+++ api/cdc.ts
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (req: VercelRequest, res: VercelResponse) => {
+  console.log(req); 
   if (req.method === 'POST') {
     return res.json({ message: "POST: Hello World!" });
   } else {
