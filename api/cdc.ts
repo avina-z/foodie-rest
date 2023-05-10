8b680db479b88d8fa76b6dd059098b97ecf3b522
Alberto Avina <avinaz@mx1.ibm.com>
2023-05-10 08:41:17 -0600

    fix import

--- api/cdc.ts
+++ api/cdc.ts
@@ -1,12 +1,12 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const writeToDatabase = require("_writeToDatabase");
+const writeToDatabase = require("./_writeToDatabase.ts");
 
 export default (req: VercelRequest, res: VercelResponse) => {
   const { body } = req;
   console.log(body); 
   if (req.method === 'POST') {
-    writeToDatabase(body);
+    writeToDatabase._writeToDatabase(body);
     return res.json({ message: "POST: Hello World!" });
   } else {
     return res.json({ message: "GET method not supported!" });
